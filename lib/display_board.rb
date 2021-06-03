c7a027fc0e7d82a5cbf52ed90dd4dd9ccfc67f4e
Garren Goudy <garren.goudy@yahoo.com>
2021-06-03 14:59:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-   puts row = ["   " "|" "   " "|" "   "]
+   puts {row1} = ["   " "|" "   " "|" "   "]
    puts separator = "-----------"
    puts row = ["   " "|" "   " "|" "   "]
    puts separator = "-----------"
@@ -12,7 +12,7 @@ def display_board(board)
   
   board = ["   ","   ","   ","   ","   ","   ","   ","   ","   ","   "]
   
-  a = 
+  row1 = [" O " "|" "   " "|" "   "]
   
   
   
