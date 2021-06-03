d6dcb89511eb3f974ab38c7b650645987f51985e
Garren Goudy <garren.goudy@yahoo.com>
2021-06-03 14:35:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ def display_board(board)
    puts separator = "-----------"
    puts row = ["   " "|" " X " "|" "   "]
    puts separator = "-----------"
-   puts row
+   puts row = ["   " "|" "   " "|" "   "]
   end
  
   
